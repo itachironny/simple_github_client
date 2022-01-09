7dd77384ff91d63ff0fe25d12f65ab18b95f6574
rounak adhikary <rounakadhikary1998@gmail.com>
2022-01-09 18:34:11 +0530

    Trying initial git log

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'select_directory.dart';
+import 'dart:io';
 
 class ItemContainer extends StatelessWidget {
   final String text;
@@ -17,16 +18,6 @@ class ItemContainer extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -39,6 +30,8 @@ class _MyHomePageState extends State<MyHomePage> {
   List<int> fullItemList=[];
   List<String> itemList=[];
 
+  String? dir, cmdText, cmdError;
+
   _MyHomePageState(){
     for (var i = 0; i < count; i++) {
       fullItemList.add(i);
@@ -47,40 +40,53 @@ class _MyHomePageState extends State<MyHomePage> {
     fullItemList.sublist(0,51).forEach((int j){itemList.add("$j");});
   }
 
-  //void getNextRange(){
-  //  var nextStart=start+
-  //}
+  Future<int> getCmdText(String? directory) async {
+    var result = await Process.run(
+      'git', 
+      ['log'],
+      workingDirectory: directory,
+      runInShell: true,
+    );
+    String ctext = result.stdout;
+    String etext = result.stderr;
+    setState((){dir=directory;cmdText=ctext;cmdError=etext;});
+    return 0;
+  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    Widget body = ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: nextRange,
+      itemBuilder: (BuildContext context, int index){
+        return ItemContainer(text:itemList[index]);
+      }, // TODO: Add actual widget
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+    );
+    if(dir!=null) body=Container(
+      padding: const EdgeInsets.all(32),
+      child: Column(
+        children: <Widget>[
+          Text(cmdText??"NULL"),
+          const Divider(),
+          Text(cmdError??"NULL"),
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: ListView.separated(
-        padding: const EdgeInsets.all(8),
-        itemCount: nextRange,
-        itemBuilder: (BuildContext context, int index){
-          return ItemContainer(text:itemList[index]);
-        }, // TODO: Add actual widget
-        separatorBuilder: (BuildContext context, int index) => const Divider(),
-      ),
+      body: body,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push<String>(
             context,
             MaterialPageRoute(builder: (context) => SelectDirectoryApp()),
-          ).then((String? directory){
-          	setState((){itemList[0]=directory??"NULL";});
-          });
+          ).then((String? directory)=>getCmdText(directory));
         },
         tooltip: 'Add a directory',
         child: const Icon(Icons.add),
